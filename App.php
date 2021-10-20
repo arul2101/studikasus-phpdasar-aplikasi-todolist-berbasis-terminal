cf6f69e0eaac14ec97d133296275a95e0f0f9872
arul2101 <myanuarullah20@gmail.com>
2021-10-20 19:01:57 +0700

    Menghubungkan file Input.php dan menjalankan Fungsi ViewShowTodoList.php di App.php

--- App.php
+++ App.php
@@ -7,8 +7,12 @@
     require_once __DIR__ . "/View/ViewShowTodoList.php";
     require_once __DIR__ . "/View/ViewAddTodoList.php";
     require_once __DIR__ . "/View/ViewRemoveTodoList.php";
+    require_once __DIR__ . "/Helper/Input.php";
 
     echo "===========================" . PHP_EOL;
     echo "SELAMAT DATANG DI APLIKASI TODOLIST!!" . PHP_EOL;
+
+    // Menjalankan fungsi ViewShowTodoList
+    viewShowTodoList();
 
 ?>
