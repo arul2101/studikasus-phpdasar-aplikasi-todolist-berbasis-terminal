f8cc42de791f00d4f4257cbbac28de8580676632
arul2101 <myanuarullah20@gmail.com>
2021-10-20 18:55:26 +0700

    Menambahkan logic ke ViewAddTodoList.php dan menghubungkannya ke file App.php

--- App.php
+++ App.php
@@ -5,6 +5,7 @@
     require_once __DIR__ . "/BussinesLogic/AddTodoList.php";
     require_once __DIR__ . "/BussinesLogic/RemoveTodoList.php";
     require_once __DIR__ . "/View/ViewShowTodoList.php";
+    require_once __DIR__ . "/View/ViewAddTodolist.php";
 
     echo "===========================" . PHP_EOL;
     echo "SELAMAT DATANG DI APLIKASI TODOLIST!!" . PHP_EOL;
--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -2,4 +2,20 @@
 
     // View/Tampilan Tambah Todolist
 
+    require_once __DIR__ . "/../Model/TodoList.php";
+    require_once __DIR__ . "/../Helper/Input.php";
+    require_once __DIR__ . "/../BussinesLogic/AddTodoList.php";
+
+    function viewAddTodolist() {
+        echo "MENAMBAH TODO" . PHP_EOL;
+
+        $todo = input("Todo (x untuk batal) ");
+
+        if( $todo == "x" ) {
+            echo "Batal menambah Todo" . PHP_EOL;
+        } else {
+            addTodoList($todo);
+        }
+    }
+
 ?>
