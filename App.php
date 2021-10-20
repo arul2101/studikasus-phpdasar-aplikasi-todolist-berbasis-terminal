468fb2a0530d74eece727088949f536d062b9ba7
arul2101 <myanuarullah20@gmail.com>
2021-10-20 18:39:18 +0700

    Menambahkan logic ke fungsi RemoveTodoList.php dan menghubungkannya ke file App.php

--- App.php
+++ App.php
@@ -3,6 +3,7 @@
     require_once __DIR__ . "/Model/Todolist.php";
     require_once __DIR__ . "/BussinesLogic/ShowTodoList.php";
     require_once __DIR__ . "/BussinesLogic/AddTodoList.php";
+    require_once __DIR__ . "/BussinesLogic/RemoveTodoList.php";
 
     echo "===========================" . PHP_EOL;
     echo "SELAMAT DATANG DI APLIKASI TODOLIST!!" . PHP_EOL;
--- BussinesLogic/RemoveTodoList.php
+++ BussinesLogic/RemoveTodoList.php
@@ -1,8 +1,20 @@
 <?php
 
     // Logic untuk menghapus Todolist
-    function removeTodoList() {
-        
+    function removeTodoList(int $number): bool {
+        global $todolist;
+
+        if( $number > sizeof($todolist) ) {
+            return false;
+        }
+
+        for( $i = $number; $i < sizeof($todolist); $i++ ) {
+            $todolist[$i] = $todolist[$i + 1];
+        }
+
+        unset($todolist[sizeof($todolist)]);
+
+        return true;
     }
 
 ?>
