c4eb8a89c914847bc88943bb4d6dd335248c8ae7
arul2101 <myanuarullah20@gmail.com>
2021-10-20 18:52:10 +0700

    Menambahkan logic ke ViewShowTodoList.php dan menghubungkan filenya ke App.php

--- App.php
+++ App.php
@@ -4,6 +4,7 @@
     require_once __DIR__ . "/BussinesLogic/ShowTodoList.php";
     require_once __DIR__ . "/BussinesLogic/AddTodoList.php";
     require_once __DIR__ . "/BussinesLogic/RemoveTodoList.php";
+    require_once __DIR__ . "/View/ViewShowTodoList.php";
 
     echo "===========================" . PHP_EOL;
     echo "SELAMAT DATANG DI APLIKASI TODOLIST!!" . PHP_EOL;
--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -2,4 +2,38 @@
 
     // View/tampilan daftar-daftar todolist yang sudah ditambah atau dihapus
 
+    require_once __DIR__ . "/../Model/Todolist.php";
+    require_once __DIR__ . "/../BussinesLogic/ShowTodoList.php";
+    require_once __DIR__ . "/../View/ViewAddTodolist.php";
+    require_once __DIR__ . "/../View/ViewRemoveTodolist.php";
+    require_once __DIR__ . "/../Helper/Input.php";
+
+    function viewShowTodolist() {
+        while(true) {
+            showTodoList();
+
+            echo "===========================" . PHP_EOL;
+            echo "MENU" . PHP_EOL;
+            echo "1. Tambah Todo" . PHP_EOL;
+            echo "2. Hapus Todo" . PHP_EOL;
+            echo "x. Keluar" . PHP_EOL;
+            echo "===========================" . PHP_EOL;
+
+            $pilihan = input("Pilih");
+
+            if( $pilihan == "1" ) {
+                viewAddTodolist();
+            }else if( $pilihan == "2" ) {
+                viewRemoveTodolist();
+            }else if( $pilihan == "x" ) {
+                break;
+            }else{
+                echo "=====WARNING!! PILIHAN TIDAK DIMENGERTI!!=====" . PHP_EOL;
+            }
+        }
+
+        echo "Sampai Jumpa Kembali!" . PHP_EOL;
+    }
+
+
 ?>
