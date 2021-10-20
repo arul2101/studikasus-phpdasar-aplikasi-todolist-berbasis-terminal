7288dbe20b4a72c6de33743e8a3b02977897fa42
arul2101 <myanuarullah20@gmail.com>
2021-10-20 08:43:19 +0700

    Menambahkan logic ke fungsi ShowTodoList.php dan menghubungkan filenya ke App.php

--- App.php
+++ App.php
@@ -1,5 +1,9 @@
 <?php
 
-    
+    require_once __DIR__ . "/Model/Todolist.php";
+    require_once __DIR__ . "/BussinesLogic/ShowTodoList.php";
+
+    echo "===========================" . PHP_EOL;
+    echo "SELAMAT DATANG DI APLIKASI TODOLIST!!" . PHP_EOL;
 
 ?>
--- BussinesLogic/ShowTodoList.php
+++ BussinesLogic/ShowTodoList.php
@@ -2,7 +2,14 @@
 
     // Logic Untuk Menampilkan Todolist
     function showTodoList() {
-        
+        global $todolist;
+
+        echo "===========================" . PHP_EOL;
+        echo "TODOLIST : " . PHP_EOL;
+
+        foreach( $todolist as $number => $value ) {
+            echo "$number. $value" . PHP_EOL;
+        }
     }
 
 ?>
